d246c3ef90906e481fc6569b545514e1484fecad
c1m50c <terminalityms@gmail.com>
2021-11-05 22:56:33 -0500

    LinkedList Append, Clear, `from_list` 🖇

--- src/linked_list.py
+++ src/linked_list.py
@@ -32,13 +32,45 @@ class LinkedList:
     tail: Node | None
     len: int
     
-    def __init__(self) -> None:
+    def __init__(self, from_list: list[any] = [  ]) -> None:
+        """
+            ## Parameters:
+            ```py
+            from_list: list[any] = [  ] # List to create a LinkedList from.
+            ```
+        """
+        
         self.head = None
         self.tail = None
         self.len = 0
+        
+        if len(from_list) != 0:
+            for x in from_list:
+                self.push_back(x)
     
     def __len__(self) -> int:
         return self.len
+    
+    def is_empty(self) -> bool:
+        return self.head == None
+    
+    def clear(self) -> None:
+        """
+            Clears the linked list, removing all nodes and resetting its length to zero.
+            
+            ## Example:
+            ```py
+            linked_list = LinkedList(from_list=[1, 2, 3])
+            linked_list.clear()
+            assert len(linked_list) == 0
+            assert linked_list.head == None
+            assert linked_list.tail == None
+            ```
+        """
+        
+        self.head = None
+        self.tail = None
+        self.len = 0
 
     def push_front(self, value: any) -> None:
         """
@@ -91,3 +123,30 @@ class LinkedList:
             self.tail = new_node
         
         self.len += 1
+    
+    def append(self, other: LinkedList) -> None:
+        """
+            Appends a linked list to the end of this linked list, clearing the other list.
+            ## Example:
+            ```py
+            linked_list_one = LinkedList(from_list=[1, 2, 3])
+            linked_list_two = LinkedList(from_list=[4, 5, 6])
+
+            linked_list_one.append(other=linked_list_two)
+            
+            assert len(linked_list_one) == 6
+            assert linked_list_one.head.value == 1
+            assert linked_list_one.tail.value == 6
+            assert len(linked_list_two) == 0
+            ```
+        """
+        
+        if self.tail != None:
+            self.tail.next = other.head
+            other.head.prev = self.tail
+            self.tail = other.tail
+        else:
+            self.head = other.head
+            self.tail = other.tail
+        self.len += len(other)
+        other.clear()
--- src/test_linked_list.py
+++ src/test_linked_list.py
@@ -1,23 +1,56 @@
 from linked_list import LinkedList
 
 
+def test_from_list():
+    linked_list = LinkedList(from_list=[1, 2, 3])
+    assert len(linked_list) == 3
+    assert linked_list.head.value == 1
+    assert linked_list.tail.value == 3
+
+
+def test_clear():
+    linked_list = LinkedList(from_list=[1, 2, 3])
+    linked_list.clear()
+    assert len(linked_list) == 0
+    assert linked_list.head == None
+    assert linked_list.tail == None
+
+
 def test_push_front():
-    linked_list: LinkedList = LinkedList()
+    linked_list = LinkedList()
     linked_list.push_front(1)
     linked_list.push_front(2)
     linked_list.push_front(3)
     assert len(linked_list) == 3
-    assert linked_list.head.value == 3 and linked_list.tail.value == 1
+    assert linked_list.head.value == 3
+    assert linked_list.tail.value == 1
 
 
 def test_push_back():
-    linked_list: LinkedList = LinkedList()
+    linked_list = LinkedList()
     linked_list.push_back(1)
     linked_list.push_back(2)
     linked_list.push_back(3)
     assert len(linked_list) == 3
-    assert linked_list.head.value == 1 and linked_list.tail.value == 3
+    assert linked_list.head.value == 1
+    assert linked_list.tail.value == 3
 
 
-test_push_front()
-test_push_back()
+def test_append():
+    linked_list_one = LinkedList(from_list=[1, 2, 3])
+    linked_list_two = LinkedList(from_list=[4, 5, 6])
+
+    linked_list_one.append(other=linked_list_two)
+    
+    assert len(linked_list_one) == 6
+    assert linked_list_one.head.value == 1
+    assert linked_list_one.tail.value == 6
+    assert len(linked_list_two) == 0
+
+
+if __name__ == "__main__":
+    test_from_list()
+    test_clear()
+    test_push_front()
+    test_push_back()
+    test_append()
